34972c78a96466934868f9866f301ade21e87b41
Herbert-C-byte <epsylon344@gmail.com>
2025-11-02 23:54:06 +0100

    Just trying something

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
 import "./index.css";
-import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus } from 'lucide-react'
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus, User } from 'lucide-react'
 import { useState, type FormEvent } from 'react'
 
 
 function App() {
   const [isGuestsOpen, setIsGuestsOpen] = useState(false)
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(true)
+
   const [emailsToInvite, setEmailsToInvite] = useState([
     'Cassio@gmail.com'
   ])
@@ -17,6 +19,14 @@ function App() {
   function closeGuestsInput() {
     setIsGuestsOpen(false)
   }
+
+  function openConfirmTripModal() {
+    setIsConfirmTripModalOpen(true)
+  }
+  
+  function closeConfirmTripModal() {
+    setIsConfirmTripModalOpen(false)
+  }
   
   function openModal() {
     setIsModalOpen(true)
@@ -25,6 +35,7 @@ function App() {
   function closeModal() {
     setIsModalOpen(false)
   }
+  
 
   function addNewEmailToInvite(event: FormEvent<HTMLFormElement> ) {
     event.preventDefault()
@@ -151,9 +162,52 @@ function App() {
             </div>
           </div>
         )}
+
+        {isConfirmTripModalOpen && (
+          <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+            <div className="w-[640px] rounded-xl py-5 px-6 bg-zinc-900 space-y-5">
+              <div className="space-y-2">
+                <div className="flex justify-between">
+                  <h2 className="text-lg font-semibold">Confirmar criação de viagem</h2>
+                  <button type="button" onClick={closeConfirmTripModal}>
+                    <X className="size-5 text-zinc-400" />
+                  </button>
+                </div>
+                <p className="text-sm text-zinc-400">
+                  Para concluir a criação de viagem para <span className="font-semibold text-zinc-100">Florianópolis, Brasil</span> nas datas de <span className="font-semibold text-zinc-100">16 a 27 de Agosto de 2024</span> preencha seus dados abaixo:
+                </p>
+              </div>
+             
+              <form onSubmit={addNewEmailToInvite}>
+                <div className="py-2.5 px-4 flex items-center flex-1 gap-2">
+                  <User className="text-zinc-400 size-5" />
+                  <input
+                    name="name" 
+                    placeholder="Seu nome completo" 
+                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                  />
+                </div>
+
+                <div className="py-2.5 px-4 flex items-center flex-1 gap-2">
+                  <User className="text-zinc-400 size-5" />
+                  <input 
+                    type="email" 
+                    name="email" 
+                    placeholder="Seu email pessoal" 
+                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                  />
+                </div>
+
+                <button type="submit" className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 ">
+                Convidar
+                <Plus className="size-5" />
+                </button>
+              </form>
+            </div>
+          </div>
+        )}
       </div>
-    </>
-      
+    </>  
   )
 }
 
