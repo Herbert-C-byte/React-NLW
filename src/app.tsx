759fc90f525cfa35b0d1f2219edc4211d1137aea
Herbert-C-byte <epsylon344@gmail.com>
2025-12-04 22:55:29 +0100

    Vibe coding slightly

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,17 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import {CreateTripPage} from "./pages/CreateTripPage";
-import {TripDetailPage}
-let router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Root,
-    loader: loadRootData,
-    element: <h1>Hello World</h1>,
-  },
-  {jfj
-    path: "/trips/:tipsId",
-    Component: Root,
-    loader: ,
-    element: <h1>Teste</h1>,
-  },
-]);
+import CreateTripPage from "./pages/create-trip";
+import TripDetailsPage from "./pages/trip-details";
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CreateTripPage />} />
+        <Route path="/trips/:tripId" element={<TripDetailsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
